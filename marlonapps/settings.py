b1b17b0a53818848b26d190b4d58a2500d0878b5
Marlon Falcon <marlonfalcon@MacBook-Pro-de-Marlon.local>
2022-07-18 09:47:32 +0200

    C136 Account

--- marlonapps/settings.py
+++ marlonapps/settings.py
@@ -78,15 +78,19 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'marlonapps.wsgi.application'
-
+AUTH_USER_MODEL = 'accounts.Account'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db_marlonapps',
+        'HOST': 'localhost',
+        'USER': 'odoo',
+        'PASSWORD': 'x1234567890',
+        'PORT': 5432
     }
 }
 
