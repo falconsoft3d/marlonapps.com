7627bb6b4553332eda5e9194d890ad64d233e4bc
Marlon Falcon <marlonfalcon@MacBook-Pro-de-Marlon.local>
2022-07-18 13:00:39 +0200

    Listado de Productos

--- apps/store/views.py
+++ apps/store/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from apps.store.models import Product
 
 # Create your views here.
 def store(request):
-    return render(request, 'store/store.html')
+    products = Product.objects.all().filter(state=True)
+    product_count = products.count()
+    
+    context = {
+        'products' : products,
+        'product_count' : product_count
+    }
+    
+    return render(request, 'store/store.html', context)
