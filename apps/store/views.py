2e01abed4eab9116f31f5832eceaf731182d7a45
Marlon Falcon <marlonfalcon@MacBook-Pro-de-Marlon.local>
2022-07-18 23:05:49 +0200

    C166 Busqueda

--- apps/store/views.py
+++ apps/store/views.py
@@ -1,3 +1,6 @@
+from ast import keyword
+from importlib.resources import contents
+from itertools import product
 from unicodedata import category
 from django.shortcuts import get_object_or_404, render
 from apps.store.models import Product
@@ -5,7 +8,7 @@ from category.models import Category
 from carts.views import _cart_id
 from carts.models import CartItem
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
-
+from django.db.models import Q
 
 # Create your views here.
 def store(request, category_slug=None):
@@ -14,13 +17,13 @@ def store(request, category_slug=None):
     
     if category_slug != None:
         categories = get_object_or_404(Category, slug=category_slug)
-        products = Product.objects.filter(category=categories, state=True)
+        products = Product.objects.filter(category=categories, state=True).order_by('-fc')
         paginator = Paginator(products, 6)
         page = request.GET.get('page')
         paged_products = paginator.get_page(page)
         product_count = products.count()
     else:
-        products = Product.objects.all().filter(state=True)
+        products = Product.objects.all().filter(state=True).order_by('-fc')
         paginator = Paginator(products, 6)
         page = request.GET.get('page')
         paged_products = paginator.get_page(page)
@@ -47,3 +50,20 @@ def product_detail(request, category_slug, product_slug):
     }
     
     return render(request, 'store/product_detail.html', context)
+
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products = Product.objects.order_by('-fc').filter(Q(description__icontains=keyword) | Q(product_name__icontains=keyword))
+            product_count = products.count()
+        else:
+            products = Product.objects.order_by('-fc')
+            product_count = products.count()
+             
+    context = {
+        'products' : products,
+        'product_count' : product_count
+    }
+    
+    return render(request, 'store/store.html', context)
