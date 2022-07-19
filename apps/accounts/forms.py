fd3310ace53f6453c2cc880f537a829199163884
Marlon Falcon <marlonfalcon@MacBook-Pro-de-Marlon.local>
2022-07-19 17:18:07 +0200

    C176 Registro de Usuarios en la BD

--- apps/accounts/forms.py
+++ apps/accounts/forms.py
@@ -1,4 +1,5 @@
 from dataclasses import field
+from distutils.command.clean import clean
 from django import forms
 from .models import Account
 
@@ -27,3 +28,14 @@ class RegistrationForm(forms.ModelForm):
         
         for field in self.fields:
             self.fields[field].widget.attrs['class']='form-control'
+            
+            
+    def clean(self):
+        cleaned_data = super(RegistrationForm, self).clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+        
+        if password != confirm_password:
+            raise forms.ValidationError(
+                "El password no coincide"
+            )
